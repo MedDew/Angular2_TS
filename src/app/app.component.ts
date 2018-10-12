fbbb6a2e4e81f89c5e53b4f60c0947067d7c4256
Med <mehdi.gacem@versusmind.eu>
2018-10-12 11:23:01 +0200

    First component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import {Component} from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector : "app-root",
+    templateUrl : "./app.component.html",
+    styleUrls : ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'my-garage';
+
+export class AppComponent
+{
+    title = "app";
+    isEnable : boolean;
+    names : string[];
+
+    constructor()
+    {
+        this.isEnable = false;
+        this.names = ["Mehdi", "Lenny"];
+    }
+
+    toggle()
+    {
+        this.isEnable = !this.isEnable;
+    }
 }
